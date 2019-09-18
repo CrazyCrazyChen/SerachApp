43aef935e5f47de8c0f3054bb0e0a33bda26c6c1
Crazy <crazy@email.com>
2019-09-18 14:40:05 +0800

    安卓应用快捷查找启动删除

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
 
+import com.example.myapplication.adapter.MyAdapter;
 import com.example.myapplication.entity.AppInfo;
 import com.example.myapplication.util.Utils;
 
@@ -13,13 +15,27 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity" ;
 
+
+    ListView lv;
+    List<AppInfo> list;
+    MyAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
 
 
-        List<AppInfo> list = Utils.getAppList(this);
+        lv = findViewById(R.id.lv_main);
+
+        adapter = new MyAdapter(this);
+
+        list = Utils.getAppList(this);
+
+        adapter.setList(list);
+
+        lv.setAdapter(adapter);
+
         Log.d(TAG, "onCreate: "+list.toString());
     }
 }
